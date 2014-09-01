ebfca787e905795add1d1bbb6c0ec43f662dd9dc
Naeem Malik <consult_naeem@hotmail.com>
2014-09-01 10:11:10 +0100

    fix config, move to java spring config

--- src/main/java/com/sky/AppConfig.java
+++ src/main/java/com/sky/AppConfig.java
@@ -2,10 +2,13 @@ package com.sky;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan({"com.sky"})
-public class AppConfig {
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
 
 
 
